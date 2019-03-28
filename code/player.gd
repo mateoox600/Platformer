eadeb000713627fa927251e352a0d23f5864f598
Mateoox600 <matboi862@gmail.com>
2019-03-28 16:34:39 +0100

    Finish Firt Level

--- code/player.gd
+++ code/player.gd
@@ -5,6 +5,9 @@ const move_value = preload('res://data/move.gd').DATA
 var vel = Vector2()
 var jump_count = 0
 var wjump_count = 0
+var canfire = true
+var time = 10
+var fire_count = 0
 
 func _ready():
 	pass
@@ -14,17 +17,26 @@ func _physics_process(delta):
 	movement_loop()
 	vel.y += move_value[5].value * delta
 	vel = move_and_slide(vel, move_value[6].value)
-	print(move_value[7].value)
+	if position.y < -2140:
+		print("You Win !")
+	
 
 func movement_loop():
 	
 	if is_on_floor() and !is_on_wall():
 		jump_count = 0
 		wjump_count = 0
+		fire_count = 0
+		if time == 0:
+			time = 10
+			canfire = true
+		if !canfire:
+			time -= 1
 	
-	var right = Input.is_action_pressed("ui_right")
-	var left = Input.is_action_pressed("ui_left")
-	var jump = Input.is_action_just_pressed("ui_accept")
+	var right = Input.is_action_pressed("right")
+	var left = Input.is_action_pressed("left")
+	var jump = Input.is_action_just_pressed("jump")
+	var fire = Input.is_action_just_pressed("fire")
 	var dirx = int(right) - int(left)
 	
 	if dirx == -1 :
@@ -43,10 +55,18 @@ func movement_loop():
 			move_value[2].value -= 1
 			if move_value[2].value == 0:
 				move_value[1].value = 1
-	elif jump == true and wjump_count < move_value[3].value:
-		vel.y = -400
+	elif jump == true and wjump_count < move_value[3].value and is_on_wall():
+		vel.y = -200
 		wjump_count += 1
 		if wjump_count == 5:
 			move_value[4].value -= 1
 			if move_value[4].value == 0:
-				move_value[3].value = 1
+				move_value[4].value = 1
+	if fire == true and fire_count < move_value[8].value and canfire:
+		canfire = false
+		fire_count += 1
+		if fire_count == 5:
+			move_value[9].value -= 1
+			if move_value[9].value == 0:
+				move_value[9].value = 1
+		
